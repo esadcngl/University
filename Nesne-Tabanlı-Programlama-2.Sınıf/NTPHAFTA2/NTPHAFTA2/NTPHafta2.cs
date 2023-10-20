f0e9ec3e0ed0f3e575eca811e0da3749d56a294a
esadcngl <muhammedesadcingili@gmail.com>
2023-10-21 01:37:40 +0300

    NTP eklendi.

---
+++ Nesne-Tabanlı-Programlama-2.Sınıf/NTPHAFTA2/NTPHAFTA2/NTPHafta2.cs
@@ -0,0 +1,122 @@
+﻿namespace NTPHAFTA2
+{
+    internal class NTPHafta2
+    {
+        static void Main(string[] args)
+        {
+            #region Değişkenlerde Scope 
+            Console.WriteLine("Değişkenlerde Scope");
+            {
+                short x = 29;
+                Console.WriteLine(x);
+            }
+            {
+                short x = 30;
+                Console.WriteLine(x);
+            }
+            // örnek 2 
+            {
+                short y = 8;
+                {
+                    // short  y = 10; tekrar tanımlarsan hata alırsın
+                    // ama atama yapabilrsin
+                    y = 19;
+                    Console.WriteLine(y);
+                }
+            }
+            cizgiEkle();
+            #endregion
+            #region Deger Tipleri sayfa-25 örnek
+            Console.WriteLine("Deger Tipleri sayfa-25 örnek");
+            {
+                bool a = new bool(); byte a1 = new byte();
+                char a2 = new char(); decimal a3 = new decimal();
+                double a4 = new double(); float a5 = new float();
+                Console.WriteLine(a);
+                Console.WriteLine(a1);
+                Console.WriteLine(a2);
+                Console.WriteLine(a3);
+                Console.WriteLine(a4);
+                Console.WriteLine(a5);
+            }
+            cizgiEkle();
+            #endregion
+            #region Object ve String Türü
+            Console.WriteLine("Object ve String türü Sayfa-31 örnek");
+            {
+                Object nesne;
+                nesne = 10;
+                Console.WriteLine(nesne.GetType);
+                nesne = "B";
+                Console.WriteLine(nesne.GetType);
+                nesne = 8.78F;
+                Console.WriteLine(nesne.GetType);
+                nesne = false;
+                Console.WriteLine(nesne.GetType);
+                nesne = 549.34M;
+                Console.WriteLine(nesne.GetType);
+            }
+            cizgiEkle();
+
+            #endregion
+            #region var keyword
+            {
+                Console.WriteLine("var Keywordü");
+                // var compile time da değişkenin türüne atanır
+                var a = "bu bir string";
+                Console.WriteLine(a);
+                //a = 10;   yapamassın var tipindeki değişkene atanan ilk tipte aynı atamalar yapılmalı
+                a = "bu ikinci string";
+                Console.WriteLine(a);
+
+            }
+            cizgiEkle();
+            #endregion
+            #region Tür Dönüşümleri
+            {
+                //bilinçsiz tür dönüşümleri
+                Console.WriteLine("Tür Dönüşümleri");
+                byte a = 20;
+                int b;
+                b = a;
+                Console.WriteLine(b);
+
+                float f = 20f;
+                double d = f;
+                Console.WriteLine(d);
+
+                char c = 'a';
+                decimal m = c;
+                Console.WriteLine(m);
+                // Örnek sayfa 43
+                float sayi = 32.32f;
+                double sayi2 = sayi;
+                Console.WriteLine("sayi'nin değeri= " + sayi + "\nsayi2'nin değeri= " + sayi2);
+                // Bilinçli tür dönüşümleri
+                int int1 = 400;
+                byte byte1 = (byte)int1;
+                Console.WriteLine(byte1);
+                // Boxing İşlemi
+                int i = 60;
+                object o =i; //boxing
+                // Unboxing 
+                int i2 = 123;
+                object o2 = i2;
+                int j = (int)o2;
+                
+            }
+            #endregion
+
+
+        }
+
+
+        static void cizgiEkle()
+        {
+            Console.WriteLine("----------------------");
+        }
+
+    }
+
+
+}
---
+++ Nesne-Tabanlı-Programlama-2.Sınıf/NTPHAFTA3/NTPHafta3/Program.cs
@@ -0,0 +1,245 @@
+﻿using System.Threading.Channels;
+
+namespace NTPHafta3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Operatörler
+            Console.WriteLine("Operatörler");
+            // as operatörü
+            // genellikle object türündeki nesneleri farklı bir türe cevirmek için kullanılır.
+            {
+                object i = "50";
+                string s = i as string;
+                //NOT as operatörü sadece referans tipde değişkenlerde kullanılır. Çünkü eşitliğin sağında bir değer olmassa null atar değer tipli değişkenlerde bu olmaz.
+
+            }
+            // is Operatörü
+            {
+                Console.WriteLine("\nis operatoru");
+                int i = 50;
+                bool b = i is int;
+                Console.WriteLine(b);
+                cizgiEkle();
+            }
+            // bitsel değil operatörü ~  
+            {
+                //Tek operand alan operatörü bir değer içindeki bitlerin teker teker
+                // tersini alır.
+                //0000 1111 sayısının bitsel değili 1111 0000 olur.
+                Console.WriteLine("\nbitsel değil operatörü ~");
+                byte b1 = 254;
+                byte b2 = (byte)~b1;
+                Console.WriteLine(b2);
+                cizgiEkle();
+            }
+            //Ternary üçlü operatör
+            {
+                Console.WriteLine("\nTernary operatör ");
+                int sayı =12;
+                String sonuc = sayı < 10 ? "sayı 10 dan kücük" : "10 a eşit yada büyük";
+                Console.WriteLine(sonuc);
+                cizgiEkle();
+            }
+            // sizeof türün bellekte kapladığı alan
+            Console.WriteLine("\nsizeof operatörü ");
+            Console.WriteLine("byte : "+ sizeof(byte));
+            Console.WriteLine("int : "+ sizeof(int));
+            Console.WriteLine("double : "+ sizeof(double));
+            cizgiEkle();
+
+            //typeof operatörü
+            Console.WriteLine("\ntypeof öperatörü");
+            {
+                Type t;
+                int i = 14;
+                float f = 12.21F;
+                t = typeof(int);
+                Console.WriteLine(t.Name);
+                cizgiEkle();
+            }
+            //default operatörü
+            Console.WriteLine("\ndefault operatörü");
+            //tanımlanan türün default değerini yollarç
+            {
+                Console.WriteLine(default(decimal));
+                Console.WriteLine(default(int));
+                Console.WriteLine(default(bool));
+            }
+            #endregion
+            #region Koşul İfadeleri
+            // if kullanımı 
+            Console.WriteLine("\nif kullanımı:");
+            {
+                string s;int a, b, c;
+                Console.Write("Birinci sayıyı giriniz: ");
+                s = Console.ReadLine();
+                a = Int32.Parse(s);
+                Console.Write("İkinci sayıyı giriniz: ");
+                s = Console.ReadLine();
+                b = Int32.Parse(s); 
+                Console.Write("Üçüncü sayıyı giriniz: ");
+                s = Console.ReadLine();
+                c = Int32.Parse(s);
+                if (a >= b && a >= c) Console.WriteLine("En büyük "+b);
+                else if (b >= c) Console.WriteLine("En büyük "+ b);
+                else Console.WriteLine("En büyük "+c);
+                cizgiEkle();
+
+            }
+            // switch kullanımı
+            {
+                Console.WriteLine("\nSwitch kullanımı");
+                String s; int a;
+                Console.WriteLine("Kaçıncı sınıfsınız?");
+                s = Console.ReadLine();
+                a = Int32.Parse(s);
+                switch (a) 
+                {
+                    case (0):
+                        case (1):
+                        Console.WriteLine("Daha yenisiniz."); break;
+                        case (2):
+                        Console.WriteLine("Mirasçısınız"); break;
+                        case (3):
+                        Console.WriteLine("Ev sahibisiniz."); break;
+                        case (4):
+                        Console.WriteLine("Misafirsiniz");break;
+                    default:
+                        Console.WriteLine("Siz okulu uzatmışsınız");goto case 1;
+                }
+                cizgiEkle();
+            }
+            #endregion
+            #region Döngüler
+            Console.WriteLine("\nDöngüler");
+            // for döngüsü
+            { Console.WriteLine("Bir sayı girin.");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int toplam = 0;
+
+                for (int i = 1; i<= n; i++)
+                {
+                    toplam = toplam + i;
+                }
+                Console.WriteLine("1'den {0}'e kadar olan sayıların toplamı {1}'dir.",n,toplam);
+                Console.WriteLine();
+            }
+            //while döngüsü
+            {
+                int i = 0;
+                while (i < 10)
+                {
+                    Console.WriteLine("Bu döngü devam etmekte ve sayı : "+ i);
+                    i++;
+                }
+            }
+            //foreach döngüsü
+            String[] ad = { "ali", "efe", "imran", "zübeyir", "mahmut" };
+            foreach(string s in ad)
+            {
+                Console.Write(s + " ");
+            }
+            cizgiEkle();
+            #endregion
+            #region Jump Atlama deyimleri
+            Console.WriteLine("\nJump atlama deyimleri");
+            // örnek 1
+            {
+                int sayı, t = 0;
+                while (true)
+                {
+                    Console.WriteLine("Negatif bir sayı girin");
+                    sayı = Convert.ToInt32(Console.ReadLine());
+                    if(sayı == 0) break;
+                    if (sayı > 0)
+                    {
+                        Console.WriteLine("Pozitif sayı girdiniz tekrar deneyiniz..");
+                        continue;
+                    }
+                    t = t + sayı;
+                }
+                Console.WriteLine("Toplam : {0} ",t);
+            }
+            // örnek 2 
+            {
+                int m = 0;
+                int n = 10;
+            bas:
+                m++;
+                if (m <= n) goto bas;
+                else goto son;
+                son: Console.WriteLine("m= "+m);
+            }
+            cizgiEkle();
+
+            #endregion
+            #region Random Sınıfı
+            Console.WriteLine("\nRandom Sınıfı");
+            // Random rastgele sayı üretmek için bir sınıftır 
+            // kullanılması için nesnesinin oluşturulması gerekir
+            {
+                Random rnd = new Random();
+                int rastgelesayı = rnd.Next(10, 20); //10 dahil 20 hariç
+                Console.WriteLine("10 dahil 20 hariç rastgele sayı : " + rastgelesayı);
+                rastgelesayı = rnd.Next(0, 50); // 0 ile 50 arası sayı 
+                Console.WriteLine("0 ile 50 arasından rastgele sayı : " +rastgelesayı);
+                rastgelesayı = rnd.Next(); // herhangi bir rastgele sayı üretir
+                Console.WriteLine("Rastgele sayı: "+ rastgelesayı);
+                double rastgelesayı2 = rnd.NextDouble();// 0.0 ile 1 arası değer döndürür
+                Console.WriteLine("0.0 ile 1 arasından rastgele sayı: "+rastgelesayı2);
+
+
+            }
+            #endregion
+            #region Diziler
+            Console.WriteLine("\n Diziler");
+            // düzenli iki boyutlu dizilerde [,] böyle tanımlanır
+            // düzensizlerde ise [][] kullanımı mevcuttur.
+
+            { int[,] dizi = new int[4, 4]; int k = 1;
+                for (int i = 0; i < 4; i++)
+                    for (int j = 0; j < 4; j++)
+                    {
+                        dizi[i, j] = k;
+                        dizi[j, i] = k;
+                            k++;
+                    }k = 1;
+                foreach(int x in dizi)
+                {
+                    if (k % 4 == 0)
+                    {
+                        Console.Write(x + " "); 
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine(x + " "); k++;
+                    } 
+
+                }
+
+                
+                    
+            }
+
+            #endregion
+
+
+
+
+
+
+
+
+
+            static void cizgiEkle()
+            {
+                Console.WriteLine("\n------------------");
+            }
+        }
+
+    }
+}
